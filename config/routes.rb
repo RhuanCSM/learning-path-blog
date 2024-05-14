3aa6e2729d8ebf781874fca1380d082fa77cfcc4
RhuanCSM <rcsm.martin@gmail.com>
2024-05-14 11:30:09 -0300

    Create out basic blog

---
+++ config/routes.rb
@@ -0,0 +1,13 @@
+Rails.application.routes.draw do
+  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+  get "up" => "rails/health#show", as: :rails_health_check
+  
+  devise_for :users
+  resources :blog_posts
+
+  get "blog_posts", to: "blog_posts#index"
+  
+
+  # Defines the root path route ("/")
+  root "blog_posts#index", as: :root
+end
